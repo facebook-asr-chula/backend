d723b567801a2606465284baf369b3c18b01924c
Pol <pol.surakit@gmail.com>
2018-05-03 00:46:32 +0700

    add code

---
+++ back.py
@@ -0,0 +1,229 @@
+import facebook
+import requests
+from backmt import *
+
+token = getmytoken()
+graph = facebook.GraphAPI(access_token = token)
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+ 
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+
+@app.route('/asr')
+def asr():
+    x = request.args.get('x')
+    result =  str(x)
+    #return result
+    
+    keywords = {
+    			0	:['เรา', 'โพสต์'], 
+                1	:['เรา', 'เช็คอิน'],  
+                3	:['ล่าสุด', 'ถูกใจ'], 
+                4	:['ล่าสุด', 'แชร์'],
+                5	:['ล่าสุด', 'ความคิดเห็น'], 
+                6	:['ขอ', 'เพื่อน', 'เรา'], 
+                7	:['อะไร', 'เล่น', 'เฟซ'], 
+                8	:['เพื่อน', 'ของ', 'เพื่อน'],
+                9	:['หนังสือ'], 
+                10	:['เพื่ื่อน', 'โพสต์'], 
+                11	:['เพื่อน', 'เช็คอิน'], 
+                12	:['เพื่อน', 'เพจ'], 
+                13	:['ถูกใจ', 'เพจ'],
+                14	:['เพจ', 'โพสต์'], 
+                15	:['เพจ', 'เรา', 'ชอบ'] 
+                }
+
+    ret = dict()
+
+
+    if chk(keywords[0], result):
+        ret = getMyPost(graph)
+        ret['type'] = 1
+        script = 'โพสต์ล่าสุดของคุณ คือ ' + ret['data'][0]['message']
+        ret['script'] = script
+        ret['sentence'] = 'เราโพสต์อะไร'
+        
+        
+    elif chk(keywords[1], result):
+        ret = getMyLocation(graph)
+        ret['type'] = 1
+        script = 'คุณเช็คอินล่าสุด ที่ ' + ret['data'][0]['message']
+        ret['script'] = script
+        ret['sentence'] = 'เราเช็คอินที่ไหน'
+        
+    elif chk(keywords[3], result):
+        ret = getMyPostLike(graph)
+        ret['type'] = 2
+        script = 'โพสต์ล่าสุดของคุณมีคนถูกใจ ' + ret['data'][0]['count'] + ' คน'
+        ret['script'] = script
+        ret['sentence'] = 'โพสต์ล่าสุดมีคนถูกใจกี่คน'
+        
+    elif chk(keywords[4], result):
+        ret = getMyPostShare(graph) #['ล่าสุด', 'แชร์']
+        ret['type'] = 2
+        script = 'โพสต์ล่าสุดของคุณมีคนแชร์ ' + ret['data'][0]['count'] + ' คน'
+        ret['script'] = script
+        ret['sentence'] = 'โพสต์ล่าสุดมีคนแชร์กี่คน'
+        
+    elif chk(keywords[5], result):
+        ret = getPostComment(graph) #['ล่าสุด', 'ความคิดเห็น']
+        ret['type'] = 3
+        script = ""
+        for x in ret['data']:
+            try:
+                tmp = ' ' + x['name'] + 'แสดงความคิดเห็นว่า ' + x['message']
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'โพสต์ล่าสุดมีความคิดเห็นอะไร'
+        
+    elif chk(keywords[6], result):
+        ret = getFriend(graph)
+        ret['type'] = 4
+        script = "เพื่อนของคุณ ได้แก่"
+        for x in ret['data']:
+            try:
+                tmp = ' ' + x['name']
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'ขอดูเพื่อนของเราหน่อย'
+        
+    elif chk(keywords[7], result):
+        ret = getFriendDevice(graph)
+        ret['type'] = 5
+        script = ""
+        for x in ret['data']:
+            try:
+                stmp = x['name'] + ' ใช้ ' + x['count']
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'เพื่อนใช้อะไรเล่นเฟซ'
+        
+    elif chk(keywords[8], result):
+        ret = getFriendFriend(graph)
+        ret['type'] = 6
+        script = "เพื่อนของ " + ret['data'][0]['name'] + ' ได้แก่ '
+        for x in ret['data'][0]['data']:
+            try:
+                tmp = x['name'] + ' '
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'ขอดูเพื่อนของเพื่อน'
+        
+    elif chk(keywords[9], result):
+        ret = getFriendBook(graph)
+        ret['type'] = 6
+        script = ''
+        for x in ret['data']:
+            try:
+                tmp = x['name'] + ' มีหนังสือที่ชอบคือ ' + x['data'][0]['name'] + ' '
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'ขอหนังสือที่เพื่อนชอบ'
+        
+    elif chk(keywords[10], result):
+        ret = getFriendPost(graph)
+        ret['type'] = 7
+        script = ''
+        for x in ret['data']:
+            try:
+                tmp =  x['name'] + ' โพสต์ว่า ' + x['data'][0]['message'] + ' '
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'เพื่อนโพสต์อะไร'
+        
+    elif chk(keywords[11], result):
+        ret = getFriendLocation(graph)
+        ret['type'] = 7
+        script = ''
+        for x in ret['data']:
+            try:
+                tmp = x['name'] + ' เช็คอินที่ ' + x['data'][0]['message'] + ' '
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'เพื่อนเช็คอินที่ไหน'
+        
+    elif chk(keywords[12], result):
+        ret = getFriendLike(graph)
+        ret['type'] = 6
+        script = ''
+        for x in ret['data']:
+            try:
+                tmp = x['name'] + ' ชอบเพจ ' + x['data'][0]['name'] + ' '
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'เพื่อนชอบเพจอะไร'
+        
+    elif chk(keywords[13], result):
+        ret = getMyPageLike(graph)
+        ret['type'] = 5
+        script = ''
+        for x in ret['data']:
+            try:
+                tmp = 'เพจ ' + x['name'] + ' มีคนถูกใจ ' + x['data'][0]['count'] + ' คน '
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'ขอจำนวนถูกใจของเพจ'
+        
+    elif chk(keywords[14], result):
+        ret = getMyPagePost(graph)
+        ret['type'] = 7
+        script = ''
+        for x in ret['data']:
+            try:
+                tmp = 'เพจ ' + x['name'] + ' โพสต์ว่า ' + x['data'][0]['message'] + ' '
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'เพจโพสต์อะไร'
+        
+    elif chk(keywords[15], result):
+        ret = getMyPage(graph)
+        ret['type'] = 4
+        script = 'เพจที่คุณชอบ ได้แก่ '
+        for x in ret['data']:
+            try:
+                tmp = 'เพจ ' + x['name'] + ' '
+                script = script + tmp
+            except:
+                script = script
+        ret['script'] = script
+        ret['sentence'] = 'ขอเพจที่เราชอบ'
+        
+    else:
+        ret['data'] = "ขออีกที"
+        ret['type'] = 99
+    
+    return str(ret)
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ backmt.py
@@ -0,0 +1,302 @@
+import facebook
+import requests
+
+pic = ',picture.height(720).width(720){url}'
+lim = '.limit(5)'
+lim1 = '.limit(1)'
+
+def getmytoken():
+    file = open('token.txt','r')
+    token = ""
+    for line in file:
+        token = token + line
+    #print(token)
+    return token
+
+
+def getMyPost(graph):
+    event = graph.request('me?fields=name,posts'+lim1+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for d in event['posts']['data']:
+        try:
+            data = {
+                'message': d['message'],
+                'time': d['created_time']
+            }
+            ret.append(data)
+        except:
+            ret = ret
+    return {'name': name, 'pic': picture, 'data':ret}
+
+def getMyPostLike(graph):
+    event = graph.request('me?fields=name,posts'+lim1+'{message,likes}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for d in event['posts']['data']:
+        data = {
+            'message': d['message'],
+            'count':0,
+        }
+        data['count']+= len(d['likes']['data'])
+        try:
+            while(true):
+                url = d['likes']['paging']['next']
+                resp = requests.get(url)
+                d['likes'] = resp.json()
+                data['count'] += len(d['likes']['data'])
+        except:
+            data['count']+=0
+        ret.append(data)
+        
+    return {'name': name, 'pic': picture, 'data':ret}
+
+def getMyPostComment(graph):
+    event = graph.request('me?fields=name,posts'+lim1+'{message,comments'+lim+'{message,from{name'+pic+'}}}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for d in event['posts']['data']:
+        data = {
+            'message': d['message'],
+            'data': [],
+        }
+        for dd in d['comments']['data']:
+            data['data'].append({'name':dd['from']['name'], 'pic': dd['from']['picture']['data']['url'], 'message':dd['message']})
+        
+        ret.append(data)
+        
+    return {'name': name, 'pic': picture, 'data':ret}
+
+def getMyPostShare(graph):
+    event = graph.request('me?fields=name,posts'+lim1+'{message,shares}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for d in event['posts']['data']:
+        data = {
+            'message': d['message'],
+            'count':d['shares']['counts']
+        }
+        ret.append(data)
+        
+    return {'name': name, 'pic': picture, 'data':ret}
+
+def getMyLocation(graph):
+    event = graph.request('me?fields=name,tagged_places'+lim1+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for d in event['tagged_places']['data']:
+        data = {
+            'message': d['place']['name'],
+            'time': d['created_time']
+        }
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+    
+def getMyPage(graph):
+    event = graph.request('me?fields=name,likes'+lim+'{name'+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for d in event['likes']['data']:
+        data = {
+            'name': d['name'],
+            'pic': d['picture']['data']['url']
+        }
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+    
+def getMyPageLike(graph):
+    event = graph.request('me?fields=name,likes'+lim+'{name,fan_count'+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for d in event['likes']['data']:
+        data = {
+            'name': d['name'],
+            'pic': d['picture']['data']['url'],
+            'count': d['fan_count']
+        }
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+    
+def getMyPagePost(graph):
+    event = graph.request('me?fields=name,likes'+lim+'{name,posts'+lim1+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for d in event['likes']['data']:
+        posts = []
+        for p in d['posts']['data']:
+            try:
+                data = {
+                    'message': p['message'],
+                    'time': p['created_time']
+                }
+                posts.append(data)
+            except:
+                posts = posts
+        data = {
+            'name': d['name'],
+            'pic': d['picture']['data']['url'],
+            'message': posts
+        }
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+
+#me?fields=friends{picture.height(720).width(720){url}}
+#name, pic
+def getFriend(graph):
+    event = graph.request('me'+'?fields=name,friends'+lim+'{name'+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for friend in event['friends']['data']:
+        data = {
+            'name': friend['name'],
+            'pic': friend['picture']['data']['url']
+        }
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+
+#name, pic, device
+def getFriendDevice(graph):
+    event = graph.request('me'+'?fields=name,friends'+lim+'{name,devices.limit(1)'+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for friend in event['friends']['data']:
+        data = {
+            'name': friend['name'],
+            'pic': friend['picture']['data']['url'],
+        }
+        try:
+            data['count']=friend['devices'][:1][0]['os']
+        except:
+            data['count']="อะไรไม่รู้ เขาไม่บอก"
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+
+#name, pic, post{msg, time, pic(ถ้ามี)}
+def getFriendPost(graph):
+    event = graph.request('me?fields=name,friends'+lim+'{name,posts'+lim1+''+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']    
+    ret = []
+    #print(event['friends']['data'][0]['posts'])
+    for friend in event['friends']['data']:
+        data = {
+            'name': friend['name'],
+            'pic': friend['picture']['data']['url'],
+            'data' : []
+        }
+        try:
+            for post in friend['posts']['data']:
+                try:
+                    a = {'message':post['message'], 'time':post['created_time']}
+                    data['data'].append(a)
+                except:
+                    data = data
+        except:
+            ret = ret
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+
+#name, pic, book{name,pic}
+def getFriendBook(graph):
+    event = graph.request('me?fields=name,friends'+lim+'{name,books'+lim1+'{name'+pic+'}'+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for friend in event['friends']['data']:
+        data = {
+            'name': friend['name'],
+            'pic': friend['picture']['data']['url'],
+            'data' : []
+        }
+        try:
+            for book in friend['books']['data']:
+                a = {'name':book['name'], 'pic':book['picture']['data']['url']}
+                data['data'].append(a)
+        except:
+            data = data
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+
+#name, pic, friend{name,pic}
+def getFriendFriend(graph):
+    event = graph.request('me?fields=name,friends'+lim1+'{name,friends'+lim+'{name'+pic+'}'+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for friend in event['friends']['data']:
+        data = {
+            'name': friend['name'],
+            'pic': friend['picture']['data']['url'],
+            'data' : []
+        }
+        try:
+            for f in friend['friends']['data']:
+                a = {'name':f['name'], 'pic':f['picture']['data']['url']}
+                data['data'].append(a)
+        except:
+            data = data
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+ 
+def getFriendLocation(graph):
+    event = graph.request('me?fields=name,friends'+lim+'{name,tagged_places'+lim1+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']    
+    ret = []
+    #print(event['friends']['data'][0]['posts'])
+    for friend in event['friends']['data']:
+        data = {
+            'name': friend['name'],
+            'pic': friend['picture']['data']['url'],
+            'data' : []
+        }
+        try:
+            for post in friend['tagged_places']['data']:
+                try:
+                    a = {'message':post['place']['name'], 'time':post['created_time']}
+                    data['data'].append(a)
+                except:
+                    data = data
+        except:
+            ret = ret
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+    
+#name, pic, page{name,pic}
+def getFriendLike(graph):
+    event = graph.request('me?fields=name,friends'+lim+'{name,likes'+lim1+'{name'+pic+'}'+pic+'}'+pic)
+    name = event['name']
+    picture = event['picture']['data']['url']
+    ret = []
+    for friend in event['friends']['data']:
+        data = {
+            'name': friend['name'],
+            'pic': friend['picture']['data']['url'],
+            'data' : []
+        }
+        try:
+            for page in friend['likes']['data']:
+                a = {'name':page['name'], 'pic':page['picture']['data']['url']}
+                data['data'].append(a)
+        except:
+            data = data
+        ret.append(data)
+    return {'name': name, 'pic': picture, 'data':ret}
+
+def chk(keyword, result):
+    idx = 0
+    for word in keyword:
+        idx = result.find(word,idx)
+        if(idx==-1):
+            return False
+    return True
